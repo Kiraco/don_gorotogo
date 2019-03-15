55a0acb7e4da1bc43e27e8aa6cff268045ad3e82
Hector Donovan Rincon Couret <donovan@wizeline.com>
2019-03-15 15:32:52 -0600

    Added comments

--- cmd/api/types.go
+++ cmd/api/types.go
@@ -1,16 +1,19 @@
 package main
 
+//Order : the order that is saved
 type Order struct {
 	UUID  string   `json:"-"`
 	Items []Coffee `json:"Items"`
 }
 
+// Coffee : item that is added to Items array
 type Coffee struct {
 	CoffeType               string      `json:"CoffeType"`
 	Toppings                string      `json:"Toppings"`
 	PersonalizedIngredients Ingredients `json:"PersonalizedIngredients"`
 }
 
+//Ingredients : modificable ingredientes in the coffee
 type Ingredients struct {
 	Milk         string `json:"Milk"`
 	CoffeeStyle  string `json:"CoffeStyle"`
